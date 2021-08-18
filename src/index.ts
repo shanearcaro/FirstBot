7af7b81407e36ac5bca94cafff15f501bbfe6661
Shane Arcaro <smarcaro@gmail.com>
2021-08-18 17:14:01 -0400

    Can't read DM's for some reason, still working on a fix

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,26 @@ import { replyMessage } from "./commands";
 import * as AmongUs from "./amongus";
 const { token } = require("../src/config");
 
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "DIRECT_MESSAGE_TYPING"] });
 
 client.once('ready', () => {
 	console.log(`User ${client.user?.tag} has logged in.`);
 });
 
-const guild = client.guilds.cache.first();
 // Respond to commands sent within a discord server
 client.on('messageCreate', (message: Message) => {
 	console.log(`Received message: ${message.content}`);
 	replyMessage(client, message);
 
+});
+
+client.on('message', (message: Message) => {
+	console.log(`Message: ${message.content}`);
+	console.log(`Message Type: ${message.type}\n`);
+	// message.author.send(`Testing reading private messages: ${message.content}`);
+	// if (message.channel.type === 'DM') {
+		// 
+	// }
 });
 
 client.login(token);
