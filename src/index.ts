9dd5f79bc746fd715b95ed21e9917fb20056e57d
Shane Arcaro <smarcaro@gmail.com>
2021-08-18 18:44:59 -0400

    Fixed DM issue by adding DIRECT_MESSAGES intent and MESSAGE partials

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { replyMessage } from "./commands";
 import * as AmongUs from "./amongus";
 const { token } = require("../src/config");
 
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "DIRECT_MESSAGE_TYPING"] });
+// const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Client({ 
+	intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+	partials: ["MESSAGE", "CHANNEL", "GUILD_MEMBER"]
+});
 
 client.once('ready', () => {
 	console.log(`User ${client.user?.tag} has logged in.`);
@@ -14,15 +18,6 @@ client.on('messageCreate', (message: Message) => {
 	console.log(`Received message: ${message.content}`);
 	replyMessage(client, message);
 
-});
-
-client.on('message', (message: Message) => {
-	console.log(`Message: ${message.content}`);
-	console.log(`Message Type: ${message.type}\n`);
-	// message.author.send(`Testing reading private messages: ${message.content}`);
-	// if (message.channel.type === 'DM') {
-		// 
-	// }
 });
 
 client.login(token);
